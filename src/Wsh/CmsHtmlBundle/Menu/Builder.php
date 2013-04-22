18d5d8f5f83d8805ba18253baeedde954539a17c
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2013-04-22 16:21:17 +0200

    page form nearly ready

---
+++ src/Wsh/CmsHtmlBundle/Menu/Builder.php
@@ -0,0 +1,24 @@
+<?php
+namespace Wsh\CmsHtmlBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware {
+	public function mainMenu(FactoryInterface $factory, array $options)
+	{
+		$menu = $factory->createItem('root');
+		$menu->setAttribute('class', 'nav nav-tabs nav-stacked main-menu');
+
+        $menu->addChild('Dashboard', array(
+        	'route' => 'dashboard', 
+        	'label' => '<i class="fa-icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a>')
+        );
+        $menu->addChild('Content', array(
+            'uri' => '#',
+        ));
+        // ... add more children
+
+        return $menu;
+	}
+}
